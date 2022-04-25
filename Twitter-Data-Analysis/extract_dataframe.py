c77ef0f0afb9b032513b8f1987ced9a60e0387d5
tutorialcreation <tutorialcreation@gmail.com>
2022-04-25 22:38:21 +0300

    added checking retweet count and checking of hashtags and findingthe number of times a person was mentioned and finding the location of the tweet and finally combining the data and saving it into a pandas dataframe and finally a csv file

--- Twitter-Data-Analysis/extract_dataframe.py
+++ Twitter-Data-Analysis/extract_dataframe.py
@@ -1,5 +1,7 @@
+import pandas as pd
 import json
 from textblob import TextBlob
+
 
 class ExtractTweets:
     """
@@ -65,3 +67,88 @@ class ExtractTweets:
         """
         return  list(map(lambda tweet: tweet['user']['friends_count'], self.tweets))
         
+    def is_sensitive(self)->list:
+        """
+        - this function checks whether the data is sensitive or not
+        """
+        return [tweet['possibly_sensitive'] if "possibly_sensitive" in tweet.keys() else None \
+        for tweet in self.tweets]
+               
+
+    def find_favourite_count(self)->list:
+        """
+        - this function returns the amount of times the tweet has been counted
+        as favorite
+        """
+        return [tweet['retweeted_status']['favorite_count'] if 'retweeted_status' in tweet.keys() else 0 \
+        for tweet in self.tweets]
+        
+    def find_retweet_count(self)->list:
+        """
+        - this function finds how many times a tweet has been retweeted
+        """
+        return [tweet['retweeted_status']['retweet_count'] if 'retweeted_status' in tweet.keys() else 0 \
+        for tweet in self.tweets]
+        
+    
+    def find_hashtags(self) -> list:
+        """
+        return the amount of hashtags in tweets
+        """
+        return [tweet.get('entities',dict()).get('hashtags', None)
+                    for tweet in self.tweets]
+
+        
+    def find_mentions(self)->list:
+        """
+        - this function returns how many times 
+        a person was mentioned in a tweet
+        """
+        return [" , ".join([count_['screen_name']  for tweet in self.tweets for count_ in tweet['entities']['user_mentions']])]
+    
+    def find_lang(self)->list:
+        """
+        return the language used to tweet
+        """
+        return list(map(lambda tweet:tweet['lang'],self.tweets))
+
+    def find_location(self)->list:
+        """
+        returns the location in which the tweet was published
+        """
+        return [tweet['user']['location'] for tweet in self.tweets]
+        
+    def get_tweet_df(self, save=False)->pd.DataFrame:
+        """required column to be generated you should be creative and add more features"""
+        
+        columns = ['created_at', 'source', 'original_text','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
+            'original_author', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place']
+        
+        created_at = self.find_created_time()
+        source = self.find_source()
+        text = self.find_full_text()
+        polarity, subjectivity = self.find_sentiments(text)
+        lang = self.find_lang()
+        fav_count = self.find_favourite_count()
+        retweet_count = self.find_retweet_count()
+        screen_name = self.find_screen_name()
+        follower_count = self.find_followers_count()
+        friends_count = self.find_friends_count()
+        sensitivity = self.is_sensitive()
+        hashtags = self.find_hashtags()
+        mentions = self.find_mentions()
+        location = self.find_location()
+        data = zip(created_at, source, text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, sensitivity, hashtags, mentions, location)
+        df = pd.DataFrame(data=data, columns=columns)
+        
+        if save:
+            df.to_csv('processed_tweet_data.csv', index=False)
+            print('File Successfully Saved.!!!')
+            
+        return df
+
+
+if __name__ == "__main__":
+
+    extracted_tweets = ExtractTweets("C:/Users/User/Documents/DScience/Twitter-Data-Analysis-One/data/Economic_Twitter_Data.json")
+    df = extracted_tweets.get_tweet_df(save=True)
