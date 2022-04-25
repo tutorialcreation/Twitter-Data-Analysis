4d8c34f929784c6adc0c004c7eb77b0a27c8e799
tutorialcreation <tutorialcreation@gmail.com>
2022-04-25 21:57:41 +0300

    added the algorithms for finding the users friends, folllowers, screen name and the source of the tweets

--- Twitter-Data-Analysis/extract_dataframe.py
+++ Twitter-Data-Analysis/extract_dataframe.py
@@ -39,3 +39,29 @@ class ExtractTweets:
         the created time tags for when the tweet was generated
         """
         return  list(map(lambda tweet: tweet['created_at'], self.tweets))
+
+
+    def find_source(self)->list:
+        """
+        - this function returns the source of the tweet
+        """
+        return  list(map(lambda tweet: tweet['source'], self.tweets))
+        
+    def find_screen_name(self)->list:
+        """
+        - this function returns the screen name of the person who has tweeted
+        """
+        return  list(map(lambda tweet: tweet['user']['screen_name'], self.tweets))
+        
+    def find_followers_count(self)->list:
+        """
+        - this function returns the amount of followers per user
+        """
+        return  list(map(lambda tweet: tweet['user']['followers_count'], self.tweets))
+        
+    def find_friends_count(self)->list:
+        """
+        - this function returns the number of friends the user has
+        """
+        return  list(map(lambda tweet: tweet['user']['friends_count'], self.tweets))
+        
