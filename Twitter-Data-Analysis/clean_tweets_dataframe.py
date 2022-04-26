eef5c6a6749fdff20fb3e1d8fca61d08dcaac434
tutorialcreation <tutorialcreation@gmail.com>
2022-04-26 05:11:16 +0300

    added algorithms for converting the floats to numbers and for removing the non english texts

--- Twitter-Data-Analysis/clean_tweets_dataframe.py
+++ Twitter-Data-Analysis/clean_tweets_dataframe.py
@@ -34,5 +34,30 @@ class TweetCleanser:
         df['created_at'] = pd.to_datetime(df['created_at'])
         return df
     
+    def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
+        """
+        convert columns like polarity, subjectivity, retweet_count
+        favorite_count etc to numbers
+        """
+        for key in df.columns:
+            if df[key].dtype == 'float64':
+                df[key] = df[key].astype(int)
+        return df
     
-    
+    def remove_non_english_tweets(self,df:pd.DataFrame)->pd.DataFrame:
+        """
+        remove non english tweets from lang
+        """
+        df = df[df['lang'].str.contains("en")]
+        return df
+
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("/data/processed_tweet_data.csv")
+    cleanser = TweetCleanser(df)
+    cleanser.drop_unwanted_column(df)
+    cleanser.drop_duplicate(df)
+    cleanser.convert_to_datetime(df)
+    cleanser.remove_non_english_tweets(df)
+    df.to_csv("data/cleaned_data.csv")
